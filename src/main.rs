ee06ef14b1b0aa9deabedb14ea8c7e32a1fa2eeb
Vincent Bavitz <vincent@bavitz.org>
2023-11-03 04:03:11 +1100

    Allow searching BAT0 and BAT1

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
 use std::fs;
+use std::path::Path;
 
 fn main() {
     let power = get_power();
 
     if power == 0.0 {
         println!("⚡️ AC");
-        return;
+    } else {
+        println!("{:.2} W", power);
     }
-
-    println!("{:.2} W", power);
 }
 
 fn get_power() -> f64 {
-    let power_filepath = "/sys/class/power_supply/BAT0/power_now";
+    let power_filepath_bat0 = "/sys/class/power_supply/BAT0/power_now";
+    let power_filepath_bat1 = "/sys/class/power_supply/BAT1/power_now";
 
-    let contents =
-        fs::read_to_string(power_filepath).expect("Something went wrong when reading power file");
-    let trimmed_contents: f64 = contents.trim().parse().unwrap();
-    let micro_watts = trimmed_contents;
-    let power = micro_watts / 1000000.;
+    // Try BAT0 first
+    if let Ok(power) = read_power_file(power_filepath_bat0) {
+        return power;
+    }
 
-    return power;
+    // Try BAT1 next
+    if let Ok(power) = read_power_file(power_filepath_bat1) {
+        return power;
+    }
+
+    // If both fail, return 0.0
+    0.0
 }
 
+fn read_power_file(filepath: &str) -> Result<f64, ()> {
+    if !Path::new(filepath).exists() {
+        return Err(());
+    }
+
+    let contents = match fs::read_to_string(filepath) {
+        Ok(contents) => contents,
+        Err(_) => return Err(())
+    };
+
+    let trimmed_contents: f64 = match contents.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err(())
+    };
+
+    let micro_watts = trimmed_contents;
+    let power = micro_watts / 1_000_000.;
+
+    Ok(power)
+}
